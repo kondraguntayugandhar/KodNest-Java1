60b295ab08376c3351f39b97c7fa7341049b975d
yugandhar <kondraguntayugandhar254@gmail.com>
2026-07-30 12:46:42 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,6 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("welcome to the java track");
+        System.out.println("I already known the the java basics");
     }
 }
