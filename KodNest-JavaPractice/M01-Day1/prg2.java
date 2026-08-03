36c55d92cde9b30698457376259257f2f0870ba5
yugandhar <kondraguntayugandhar254@gmail.com>
2026-08-03 17:47:52 +0530

    Add Day 1 prg2.java practice program

---
+++ KodNest-JavaPractice/M01-Day1/prg2.java
@@ -0,0 +1,18 @@
+public class prg2 {
+    public static void main(String[] args) {
+
+        int solvedProblems = 47;
+        int practiceDays = 5;
+        double exactPercentage = 86.75;
+
+        double reportValue = solvedProblems;
+        int wholePercentage = (int) exactPercentage;
+        double average = (double) solvedProblems / practiceDays;
+        boolean dailyTarget = average >= 9.0;
+
+        System.out.println("Report Value: " + reportValue);
+        System.out.println("Whole Percentage: " + wholePercentage);
+        System.out.println("Average Per Day: " + average);
+        System.out.println("Daily Target Reached: " + dailyTarget);
+    }
+}
