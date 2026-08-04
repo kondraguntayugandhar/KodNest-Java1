eea8f022392c515c172dac5acaf95f4756c207d9
yugandhar <kondraguntayugandhar254@gmail.com>
2026-08-04 12:33:13 +0530

    add prg2

---
+++ KodNest-JavaPractice/M01-Day2/pos.java
@@ -0,0 +1,13 @@
+public class pos {
+    public static void main(String[] args) {
+        int num = 0;
+        if (num > 0) {
+            System.out.println("positive");
+        } else if (num < 0) {
+            System.out.println("negtive");
+        } else {
+            System.out.println("zero");
+        }
+    }
+
+}
