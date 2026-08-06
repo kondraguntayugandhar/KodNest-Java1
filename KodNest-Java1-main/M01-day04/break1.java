0f64e38f822367b023a9b082def95f8343fa23ef
yugandhar <kondraguntayugandhar254@gmail.com>
2026-08-06 12:26:51 +0530

    add day4 updates

---
+++ KodNest-Java1-main/M01-day04/break1.java
@@ -0,0 +1,11 @@
+
+public class break1 {
+    public static void main(String[] arge) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 6) {
+                break;
+            }
+        }
+    }
+}
